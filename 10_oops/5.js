a09dd4085aedf3f92d63a2c3de7c24e2bb2fe99e
Nitin0069 <nitin4rana@gmail.com>
2026-08-24 11:18:47 -0700

    this

---
+++ 10_oops/5.js
@@ -0,0 +1,14 @@
+ function multiply5 (num) {
+    return num * 5
+ }
+multiply5.power = "budhu"
+
+ const num = multiply5(5);
+console.log(num);
+console.log(multiply5.power)
+console.log(multiply5.prototype) 
+
+
+function createUser (username, score) {
+    
+}
