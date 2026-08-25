059a23c6899cb7ae9bb592accd6ba9b1cc0db5fc
Nitin0069 <nitin4rana@gmail.com>
2026-08-25 10:20:23 -0700

    mnewwww

--- 10_oops/5.js
+++ 10_oops/5.js
@@ -1,3 +1,5 @@
+ 
+ /*
  function multiply5 (num) {
     return num * 5
  }
@@ -10,5 +12,38 @@ console.log(multiply5.prototype)
 
 
 function createUser (username, score) {
-    
 }
+*/
+/*
+function createe (username,roll) {
+    this.createe = createe;
+this.roll = roll;
+}
+
+createe.prototype.aage = function () {
+    this.roll++
+}
+const chai = createe("chai", 69)
+const tea = createe("tea", 250)
+chai.aage();
+console.log(createe.prototype.aage )
+*/
+
+function createUsr (username, kaam) {
+    this.username = username
+    this.kaam = kaam
+
+}
+
+createUsr.prototype.chal = function () {
+    this.kaam++
+}
+createUsr.prototype.printMe = function() {
+    console.log(`price is ${this.kaam}`)
+}
+const chai = new createUsr("chai", 25)
+const tea = new createUsr("tea", 250)
+
+chai.printMe()
+chai.chal()
+
