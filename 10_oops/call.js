ff9ae561f77bca478b243333b31c38462ffdffd0
Nitin0069 <nitin4rana@gmail.com>
2026-08-28 18:05:53 +0530

    call and bind confusion khtml

---
+++ 10_oops/call.js
@@ -0,0 +1,23 @@
+let name = {
+    firstname: "nitin",
+    lastname: "rana",
+   
+    }
+let printfull = function () {
+        console.log(this.firstname + " " + this.lastname) 
+    }
+
+
+
+
+let name2 = {
+    firstname: "nakshu",
+    lastname: "rana"
+}
+//name.printfull.call(name2);
+printfull.call(name2);
+
+
+//apply
+ let prit = printfull.bind(name);
+ prit();
