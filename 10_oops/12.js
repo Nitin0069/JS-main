dedef2d35432924b9dc57be074039c801909e6ef
Nitin0069 <nitin4rana@gmail.com>
2026-08-30 14:02:15 +0530

    getter and setter property base

--- 10_oops/12.js
+++ 10_oops/12.js
@@ -32,7 +32,7 @@ console.log(hitesh.password)
 */
 
 
-
+/*
 class User {
     constructor (name) {
         this.name = name
@@ -53,3 +53,77 @@ class User {
 }
 const nitu = new User ("n");
 console.log(nitu.name)
+
+*/
+/*
+class User {
+    constructor (email, password) {
+        this.email = email;
+        this.password = password
+    }
+
+get email () {
+    return this._email
+}
+set email (yo){
+    this._email = yo.toUpperCase();
+}
+
+ get password() {
+    return this._password
+ }
+ set password(value) {
+    this._password = value.toUpperCase ()
+ }
+}
+
+
+
+
+const hitesh = new User("hitesh@", "abc")
+console.log(hitesh.password);
+console.log(hitesh.email)
+
+*/
+
+
+
+
+/*
+class  naam {
+constructor(name){
+    this.name = name
+
+}
+get name () {
+    return this._name
+}
+set name (value) {
+    this._name = value.toUpperCase()
+}
+}
+
+const chalo = new naam ("nitin");
+console.log(chalo.name);
+chalo.name = "nakshu"
+console.log(chalo.name)
+*/
+
+
+function user  (email, password) {
+this._email = email;
+this._password = password;
+
+Object.defineProperty (this, "email", {
+    get: function () {
+        return this._email.toUpperCase()
+    },
+    set: function (value) {
+this._email = value
+    }
+
+})
+
+}
+const obj = new user ("chal");
+console.log(obj.email);
