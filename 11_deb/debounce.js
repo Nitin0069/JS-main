20fbcbbf2572a09fa509acaa814712ff8f6993f2
Nitin0069 <nitin4rana@gmail.com>
2026-09-03 22:18:29 +0530

    debiunce

--- 11_deb/debounce.js
+++ 11_deb/debounce.js
@@ -33,10 +33,9 @@ const debouncedSearch = debounce(search, 1000);
 
 
 */
-
+/*
 function debounce  (fn, delay) {
 let timerId;
-
 return function (...args) {
     clearTimeout(timerId);
    timerId =  setTimeout (function () {
@@ -45,13 +44,26 @@ return function (...args) {
 
 }
 }
-
 const search = function (query) {
     console.log(`searching for ` , query)
 }
-
-
 const searchWithDebounce = debounce(search, 500)
+searchWithDebounce("laptop")
+searchWithDebounce("laptop")
+*/
 
-searchWithDebounce("laptop")
-searchWithDebounce("laptop")
+function debounce (fn, delay) {
+let timerr ;
+return function (...args) {
+  clearTimeout(timerr)
+  timerr     =   setTimeout(function () {
+fn.apply(this, args);
+    },delay)
+}
+}
+const  mouse = function( hila) {
+    console.log("mouse hila" , hila  )
+}
+const experiment = debounce (mouse, 2000)
+experiment("ninini")
+experiment("jhihihi")
