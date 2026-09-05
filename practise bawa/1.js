3af032d5e7d3684db2e903420e388ef5c14e3b32
Nitin0069 <nitin4rana@gmail.com>
2026-09-05 12:30:53 +0530

    objects complete

--- practise bawa/1.js
+++ practise bawa/1.js
@@ -7,7 +7,7 @@ chai();
 
 
 // ham bnayenge  die
-
+/*
 let dice = Math.random()
 
 console.log(Math.floor(dice*6 + 1))
@@ -15,11 +15,47 @@ console.log(Math.floor(dice*6 + 1))
 
 
 
+const user = {
+    name: "Nitin",
+    greeting: function () {
+        console.log("chal pada maachoo")
+        return this.name
+
+    }
+}
+
+console.log(user.greeting())
+
+user.name = "badal gya"
+user.name = "badal gya part 2"
+console.log(user.name)
+
+//combining objects
+
+const obj = {
+    1: "a",
+    2: "b",
+    3: "c"
+}
+const obj2 = {
+    4: "d",
+    5: "e",
+}
 
 
+ const obj3 = Object.assign ({},ibj , obj2);
+
+ // object destructuring
 
 
+const nimbu = {
+    name: "nnitin",
+    age: 21,
+    city: "chandiigarh"
+};
+const {name,age,city} = nimbu;
+*/
 
-
+/// function 
 
 
