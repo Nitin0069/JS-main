5100cbd42f071698403a28afd5f4a5dd1ae470f6
Nitin0069 <nitin4rana@gmail.com>
2026-09-06 23:15:30 +0530

    practise

---
+++ practise bawa/4.js
@@ -0,0 +1,34 @@
+/*console.log("loop chal pada")
+
+
+for (let i = 0; i < 5; i++ ) {
+    console.log("chal shonu")
+}
+
+
+const users = ["Nitin", "Rahul", "Aman", "Raj", "sham", "ram"];
+
+for (let i = 0 ; i <= users.length; i++) {
+    console.log(users[i])
+}
+
+
+let i = 0; 
+while (i < 5) {
+    console.log(i)
+    i++; 
+
+}
+
+
+let j = 0;
+do {
+    console.log(i);
+    i++;
+} while (j < 5);
+
+*/
+
+
+const fruits = ["Apple", "Banana", "Mango"];
+console.log(fruits[i]);
